605cbc12c7546287fbeec51294b3ff9053a0f64f
Laurine Ndanu Nzuki <nzukilaurine@gmail.com>
2024-07-10 01:04:58 +0300

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+findMatching(drivers);
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+    }).sort();
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().indexOf(name.toLowerCase()) === 0;
+    }).sort();
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
